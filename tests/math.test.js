e0a52960ad848ae0e96f1b450678c023ad5fc351
Jon <jong649@gmail.com>
2020-05-15 00:00:51 -0700

    More simple tests using Jest

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require("../src/math");
+const {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+} = require("../src/math");
 
 test("Should calculate total with tip", () => {
   const total = calculateTip(10, 0.3);
@@ -8,4 +12,14 @@ test("Should calculate total with tip", () => {
 test("Should calculate total with default tip", () => {
   const total = calculateTip(10);
   expect(total).toBe(12.5);
+});
+
+test("Should convert 32F to 0C", () => {
+  const temp = fahrenheitToCelsius(32);
+  expect(temp).toBe(0);
+});
+
+test("Should convert 0C to 32F", () => {
+  const temp = celsiusToFahrenheit(0);
+  expect(temp).toBe(32);
 });
